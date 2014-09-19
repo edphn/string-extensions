64f63e70631fc6867bef0fe768c1463639b7e4cb
Jacek Przygoda <jacek.przygoda@gmail.com>
2014-09-19 23:28:48 +0200

    Initial repository.

---
+++ StringExtensions/Extensions.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringExtensions
+{
+    public static class Extensions
+    {
+        public static char At(this string instance, int index)
+        {
+            return instance[index];
+        }
+
+        public static string Camelize(this string instance)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string CollapseWhitespace(this string instance)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static int CountrSubstring(this string instance)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string Dasherize(this string instance)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string EnsureLeft(this string instance)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string EnsureRight(this string instance)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string Humanize(this string instance)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool IsAlpha(this string instance)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool IsAlphanumeric(this string intance)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool IsHexadecimal(this string intance)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool IsJson(this string intance)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool IsLowerCase(this string intance)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool IsUpperCase(this string intance)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string LowerCaseFirst(this string intance)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string RemoveLeft(this string instance)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string RemoveRight(this string instance)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string SafeTruncate(this string instance)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string Shuffle(this string instance)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string Slugify(this string instance)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string Surround(this string instance)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string SwapCase(this string instance)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string Titleize(this string instance)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string ToTitleCase(this string instance)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string Truncate(this string instance)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string UpperCaseFirst(this string instance)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string Undercored(this string instance)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
